4b6320dca3a6e5961626a721afcfd4e873997d2e
Soew <55360932+Lousio-n@users.noreply.github.com>
2023-09-25 22:33:46 +0200

    add simple id for books and checkbox for read status

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-const mainContent = document.querySelector('#mainContent');
+const mainContainer = document.querySelector('.mainContainer');
+const bookShelf = document.querySelector('.bookShelf')
 const myLibrary = [];
+let bookId = 1;
 
-function Book(title, author, pages, status) {
+function Book(id, title, author, pages, status) {
+    this.id = id
     this.title = title
     this.author = author
     this.pages = pages
     this.status = status
 }
 
-function addBook(title, author, pages, status) {
-    const bookToAdd = new Book(title, author, pages, status);
+function addBook(id, title, author, pages, status) {
+    const bookToAdd = new Book(id, title, author, pages, status);
     myLibrary.push(bookToAdd);
+    bookId++
 }
 
-addBook('lotr', 'tolkien', 55, 'yes');
-addBook('asd', 'ssd', 23, 'no');
+addBook(bookId, 'lotr', 'tolkien', 55, true);
+addBook(bookId, 'asd', 'ssd', 23, false);
 
 
 function showAllBooks() {
     myLibrary.forEach((book) => {
+        const id = `book${book.id}`;
+
+        const lable = document.createElement('lable');
+        lable.setAttribute("for", id);
+
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.name = "readStatus";
+        checkbox.value = book.status
+        checkbox.className = id;
+
+        if (book.status === true) checkbox.checked = true;
+
+        lable.appendChild(checkbox);
+        lable.appendChild(document.createTextNode("Read Status"));
+        
         const bookCard = document.createElement('div');
         bookCard.classList.add('bookCard');
-        bookCard.textContent = `${book.title} by ${book.author} is ${book.pages} pages.`;
-        mainContent.appendChild(bookCard);
+        bookCard.textContent = `Title: ${book.title} Author: ${book.author} Pages: ${book.pages}`;
+        bookCard.appendChild(lable);
+
+        bookShelf.appendChild(bookCard);
     })
 }
 
